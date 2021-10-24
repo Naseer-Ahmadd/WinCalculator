023a88676a80823e9313df3cb6b8b03d385149de
Naseer-Ahmadd <naseerrather58@gmail.com>
2021-10-24 19:19:59 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,337 @@
+﻿
+namespace WinCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.n1 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.badd = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.bsub = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.bmul = new System.Windows.Forms.Button();
+            this.ndot = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.bc = new System.Windows.Forms.Button();
+            this.bdiv = new System.Windows.Forms.Button();
+            this.nequal = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textBox1.Font = new System.Drawing.Font("Courier New", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(-2, 0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(293, 40);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "0";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // n1
+            // 
+            this.n1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.n1.Font = new System.Drawing.Font("Courier New", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n1.Location = new System.Drawing.Point(11, 47);
+            this.n1.Name = "n1";
+            this.n1.Size = new System.Drawing.Size(62, 37);
+            this.n1.TabIndex = 1;
+            this.n1.Text = "1";
+            this.n1.UseVisualStyleBackColor = false;
+            this.n1.Click += new System.EventHandler(this.n1_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button1.Font = new System.Drawing.Font("Courier New", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(79, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(62, 37);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "2";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button2.Font = new System.Drawing.Font("Courier New", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(147, 47);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(62, 37);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "3";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // badd
+            // 
+            this.badd.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.badd.Font = new System.Drawing.Font("Courier New", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.badd.Location = new System.Drawing.Point(215, 47);
+            this.badd.Name = "badd";
+            this.badd.Size = new System.Drawing.Size(62, 37);
+            this.badd.TabIndex = 4;
+            this.badd.Text = "+";
+            this.badd.UseVisualStyleBackColor = false;
+            this.badd.Click += new System.EventHandler(this.badd_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button4.Font = new System.Drawing.Font("Courier New", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(11, 90);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(62, 37);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button5.Font = new System.Drawing.Font("Courier New", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(79, 90);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(62, 37);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button6.Font = new System.Drawing.Font("Courier New", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(147, 90);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(62, 37);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // bsub
+            // 
+            this.bsub.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.bsub.Font = new System.Drawing.Font("Courier New", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bsub.Location = new System.Drawing.Point(214, 90);
+            this.bsub.Name = "bsub";
+            this.bsub.Size = new System.Drawing.Size(62, 37);
+            this.bsub.TabIndex = 8;
+            this.bsub.Text = "-";
+            this.bsub.UseVisualStyleBackColor = false;
+            this.bsub.Click += new System.EventHandler(this.bsub_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button8.Font = new System.Drawing.Font("Courier New", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(11, 133);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(62, 37);
+            this.button8.TabIndex = 9;
+            this.button8.Text = "7";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button9.Font = new System.Drawing.Font("Courier New", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(79, 133);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(62, 37);
+            this.button9.TabIndex = 10;
+            this.button9.Text = "8";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button10.Font = new System.Drawing.Font("Courier New", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button10.Location = new System.Drawing.Point(146, 133);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(62, 37);
+            this.button10.TabIndex = 11;
+            this.button10.Text = "9";
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // bmul
+            // 
+            this.bmul.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.bmul.Font = new System.Drawing.Font("Courier New", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bmul.Location = new System.Drawing.Point(215, 133);
+            this.bmul.Name = "bmul";
+            this.bmul.Size = new System.Drawing.Size(62, 37);
+            this.bmul.TabIndex = 12;
+            this.bmul.Text = "*";
+            this.bmul.UseVisualStyleBackColor = false;
+            this.bmul.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // ndot
+            // 
+            this.ndot.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ndot.Font = new System.Drawing.Font("Courier New", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ndot.Location = new System.Drawing.Point(11, 176);
+            this.ndot.Name = "ndot";
+            this.ndot.Size = new System.Drawing.Size(62, 37);
+            this.ndot.TabIndex = 13;
+            this.ndot.Text = ".";
+            this.ndot.UseVisualStyleBackColor = false;
+            this.ndot.Click += new System.EventHandler(this.ndot_Click);
+            // 
+            // button13
+            // 
+            this.button13.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button13.Font = new System.Drawing.Font("Courier New", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button13.Location = new System.Drawing.Point(79, 176);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(62, 37);
+            this.button13.TabIndex = 14;
+            this.button13.Text = "0";
+            this.button13.UseVisualStyleBackColor = false;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // bc
+            // 
+            this.bc.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.bc.Font = new System.Drawing.Font("Courier New", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bc.Location = new System.Drawing.Point(147, 176);
+            this.bc.Name = "bc";
+            this.bc.Size = new System.Drawing.Size(62, 37);
+            this.bc.TabIndex = 15;
+            this.bc.Text = "C";
+            this.bc.UseVisualStyleBackColor = false;
+            this.bc.Click += new System.EventHandler(this.bc_Click);
+            // 
+            // bdiv
+            // 
+            this.bdiv.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.bdiv.Font = new System.Drawing.Font("Courier New", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bdiv.Location = new System.Drawing.Point(215, 176);
+            this.bdiv.Name = "bdiv";
+            this.bdiv.Size = new System.Drawing.Size(62, 37);
+            this.bdiv.TabIndex = 16;
+            this.bdiv.Text = "/";
+            this.bdiv.UseVisualStyleBackColor = false;
+            this.bdiv.Click += new System.EventHandler(this.bdiv_Click);
+            // 
+            // nequal
+            // 
+            this.nequal.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.nequal.Font = new System.Drawing.Font("Courier New", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nequal.Location = new System.Drawing.Point(54, 219);
+            this.nequal.Name = "nequal";
+            this.nequal.Size = new System.Drawing.Size(131, 32);
+            this.nequal.TabIndex = 17;
+            this.nequal.Text = "=";
+            this.nequal.UseVisualStyleBackColor = false;
+            this.nequal.Click += new System.EventHandler(this.nequal_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Font = new System.Drawing.Font("Edwardian Script ITC", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(164, 264);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 25);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Created by Naseer";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(289, 298);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nequal);
+            this.Controls.Add(this.bdiv);
+            this.Controls.Add(this.bc);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.ndot);
+            this.Controls.Add(this.bmul);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.bsub);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.badd);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.n1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button n1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button badd;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button bsub;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button bmul;
+        private System.Windows.Forms.Button ndot;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button bc;
+        private System.Windows.Forms.Button bdiv;
+        private System.Windows.Forms.Button nequal;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
